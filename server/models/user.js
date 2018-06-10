7effe95892694e59edbd84be1eb79c416634f699
prashant075 <guptaprashant075.com>
2018-06-10 09:39:25 +0530

    add sing x-auth credential

--- server/models/user.js
+++ server/models/user.js
@@ -68,6 +68,26 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 };
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
 
 UserSchema.pre('save', function (next) {
   var user = this;
@@ -83,6 +103,8 @@ UserSchema.pre('save', function (next) {
     next();
   }
 });
+
+
 
 var User = mongoose.model('User', UserSchema);
 
